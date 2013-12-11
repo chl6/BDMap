2e7447d644411454a0b04d81b61581863cb7df17
深圳鲲鹏 <apple@shinsenkunpengmatoiMac-149.local>
2013-12-11 14:19:37 +0800

    Initial Commit

---
+++ BMap/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BMap
+//
+//  Created by 深圳鲲鹏 on 13-12-11.
+//  Copyright (c) 2013年 lihu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BMap/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BMap
+//
+//  Created by 深圳鲲鹏 on 13-12-11.
+//  Copyright (c) 2013年 lihu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
